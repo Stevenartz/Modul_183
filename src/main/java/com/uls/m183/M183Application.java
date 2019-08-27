18501c37b83e35b561cc7118ded16e26a262ece3
Stefan <s.ulrich31@gmx.ch>
2019-08-27 10:22:37 +0200

    Initial Commit

---
+++ src/main/java/com/uls/m183/M183Application.java
@@ -0,0 +1,14 @@
+package com.uls.m183;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M183Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M183Application.class, args);
+		System.out.println(">>> Test");
+	}
+
+}
---
+++ src/test/java/com/uls/m183/M183ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uls.m183;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class M183ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
